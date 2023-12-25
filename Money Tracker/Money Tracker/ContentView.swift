7e891436bebca44537eb2d771042f49bb6dadb2c
Matthieu Déglon <mdeglon@alphacoders.io>
2023-12-25 09:19:14 +0100

    initial commit

---
+++ Money Tracker/Money Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Money Tracker
+//
+//  Created by Matthieu Déglon on 25/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Money Tracker/Money Tracker/Money_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_TrackerApp.swift
+//  Money Tracker
+//
+//  Created by Matthieu Déglon on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Money_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
